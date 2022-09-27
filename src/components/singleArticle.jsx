e6ec2b8ea3a859b7c4e7e7bf32f842c2831a2349
David Peake <david.peake@skybettingandgaming.com>
2022-09-27 11:07:06 +0100

    task 7

--- src/components/singleArticle.jsx
+++ src/components/singleArticle.jsx
@@ -1,12 +1,13 @@
 import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { getSingleArticle } from "../utils/api";
+import { getSingleArticle, updateVotes } from "../utils/api";
 
 
 const SingleArticle = () => {
 
     const [currentArticle, setCurrentArticle] = useState({})
     const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
     let {article_id} = useParams()
 
     useEffect(() => {
@@ -17,7 +18,34 @@ const SingleArticle = () => {
         })
     })
 
+    const increaseVoteCount = () => {
+        console.log(currentArticle)
+        setCurrentArticle({
+            article_id: currentArticle.article_id,
+            author: currentArticle.author,
+            body: currentArticle.body,
+            comment_count: currentArticle.comment_count,
+            created_at: currentArticle.created_at,
+            title: currentArticle.title,
+            topic: currentArticle.topic,
+            votes: currentArticle.votes+1
+        })
+        console.log(currentArticle.votes)
+    }
+
+    const IncreaseVote = () => {
+            increaseVoteCount()
+            updateVotes(1, article_id)
+            .catch((err) => {
+                setIsError(true)
+            })
+            
+    }
+
+
+
     if(isLoading) return <p>Loading Article...</p>
+    if(isError) return <p>Ooops...Something went wrong</p>
     
         return (
             <article className="article">
@@ -26,6 +54,7 @@ const SingleArticle = () => {
                 <h4>{currentArticle.body}</h4>
                 <p>Date published: {currentArticle.created_at}</p>
                 <p>Votes: {currentArticle.votes}</p>
+                <button onClick={IncreaseVote}>Vote</button>
                 <p>Comments: {currentArticle.comment_count}</p>
                     <p><Link to="/articles">Return to all articles</Link></p>
             </article>      
